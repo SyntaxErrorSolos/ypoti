63bfd519c275c5c76291b523d36da113c3590e94
NotYourAbu <68179601+SyntaxErrorSolos@users.noreply.github.com>
2023-09-01 22:15:16 +0500

    GUI and color change.
    Added a basic GUI

--- Frontend/app/page.js
+++ Frontend/app/page.js
@@ -2,10 +2,19 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <canvas width={512} height={512} className='bg-white'>
-
-      </canvas>
+    <div className='flex h-screen'>
+      <div className='bg-green-500 rounded-lg text-center h-44'>
+          <h1 className='font-bold text-3xl text-black'>
+            Information
+          </h1>
+          <input type='text' className='bg-white' placeholder='Your username'>
+          </input>
+          <input type='color'></input>
+      </div>
+      <div className='m-auto'>
+        <canvas width={512} height={512} className='bg-white'>
+        </canvas>
+      </div>
     </div>
   )
 }
