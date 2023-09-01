93645404c3d63ef6135064140b9e9ff4e2de5243
NotYourAbu <68179601+SyntaxErrorSolos@users.noreply.github.com>
2023-09-01 21:00:31 +0500

    Creating the canvas
    Added the basic canvas

---
+++ Frontend/app/page.js
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <canvas width={512} height={512} className='bg-white'>
+
+      </canvas>
+    </div>
+  )
+}
